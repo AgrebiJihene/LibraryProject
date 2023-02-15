7b23ca0f001f16e9deca5cf67cc9bebe0e596067
AgrebiJihene <jihene.agrebi@etu.univ-cotedazur.fr>
2023-02-15 11:29:04 +0100

    DB init

--- ASP.Server/Database/DBInitializer.cs
+++ ASP.Server/Database/DBInitializer.cs
@@ -19,20 +19,20 @@ namespace ASP.Server.Database
 
             Genre SF, Classic, Romance, Thriller;
             bookDbContext.Genre.AddRange(
-                SF = new Genre(),
-                Classic = new Genre(),
-                Romance = new Genre(),
-                Thriller = new Genre()
+                SF = new Genre() { Name="SF"},
+                Classic = new Genre() { Name = "Classic" },
+                Romance = new Genre() { Name = "Romance" },
+                Thriller = new Genre() { Name = "Thriller" }
             );
             bookDbContext.SaveChanges();
 
             // Une fois les moèles complété Vous pouvez faire directement
             // new Book() { Author = "xxx", Name = "yyy", Price = n.nnf, Content = "ccc", Genres = new() { Romance, Thriller } }
             bookDbContext.Books.AddRange(
-                new Book(), 
-                new Book(),
-                new Book(),
-                new Book()
+                new Book() { Title ="book1", Content ="content1", Price = 10.6, Genres= new() {Romance,Thriller} },
+                new Book() { Title = "book2", Content = "content2", Price = 28.6, Genres = new() {SF, Thriller } },
+                new Book() { Title = "book3", Content = "content3", Price = 74.6, Genres = new() {Classic, Thriller } },
+                new Book() { Title ="book4", Content ="content4", Price = 18.7, Genres= new() {Romance,Classic} }
             );
             // Vous pouvez initialiser la BDD ici
 
